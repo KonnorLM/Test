be88dbe4af1cfc665ed7f218bdae49f991e3800f
KonnorLM <43870071+KonnorLM@users.noreply.github.com>
2019-04-24 19:32:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NTcwMzkzNzMwOTU2MTMyMzUz.XMEFsQ.dJTLDH0f6oON8na9HL80eGi3Wj0);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
